e4a3e9926154209740209da65a9cfd2dc3353e1b
Jimmy <itsjimmyh@gmail.com>
2014-09-21 22:45:39 -0700

    click anywhere on errors will hide errors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,14 @@
 //= require underscore
 //= require backbone
 //= require bootstrap.min
+//= require underscore
+//= require backbone
+//= require fixie_bnb
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
 
 $(function () {
---
+++ app/assets/javascripts/fixie_bnb.js
@@ -0,0 +1,13 @@
+window.FixieBNB = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  FixieBNB.initialize();
+});
