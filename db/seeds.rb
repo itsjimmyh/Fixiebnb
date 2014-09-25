d48aebe55f784348082cb2508c195c25503877a2
Jimmy <itsjimmyh@gmail.com>
2014-09-24 20:29:42 -0700

    img's display.  list bike's info

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,13 +21,18 @@
 #  created_at :datetime
 #  updated_at :datetime
 
-User.create!( username: "username", password: "123456")
-User.create!( username: "eric", password: "123456")
-User.create!( username: "mike", password: "123456")
+User.create!( username: "username", password: "123456" )
+User.create!( username: "eric", password: "123456" )
+User.create!( username: "mike", password: "123456" )
+User.create!( username: "rob", password: "123456" )
+User.create!( username: "jeffrey", password: "123456" )
+User.create!( username: "yusef", password: "123456" )
+User.create!( username: "james", password: "123456" )
+User.create!( username: "nguyen", password: "123456" )
 
 Listing.create!(
   user_id: "1",
-  list_title: "jimmy's bianchi pista concept",
+  list_title: "jimmy's bianchi concept",
   list_desc: "Please describe your ride",
   price: 25,
   lat: 37.781661,
@@ -40,7 +45,7 @@ Listing.create!(
 
 Listing.create!(
   user_id: "2",
-  list_title: "eric's concept",
+  list_title: "eric's njs",
   list_desc: "Please describe your ride",
   price: 25,
   lat: 37.781661,
@@ -53,7 +58,7 @@ Listing.create!(
 
 Listing.create!(
   user_id: "3",
-  list_title: "mike's concept",
+  list_title: "mike's sweet ride",
   list_desc: "Please describe your ride",
   price: 25,
   lat: 37.781661,
@@ -64,18 +69,44 @@ Listing.create!(
   zip: 94103
 )
 
-# Listing.create!(
-#   user_id: "3",
-#   list_title: "mike's concept",
-#   list_desc: "Please describe your ride",
-#   price: 25,
-#   lat: 37.781661,
-#   long: -122.411579,
-#   street: "1061 Market St",
-#   city: "San Francisco",
-#   state: "CA",
-#   zip: 94103
-# )
+Listing.create!(
+  user_id: "4",
+  list_title: "robs's concept",
+  list_desc: "Please describe your ride",
+  price: 25,
+  lat: 37.781661,
+  long: -122.411579,
+  street: "1061 Market St",
+  city: "San Francisco",
+  state: "CA",
+  zip: 94103
+)
+
+Listing.create!(
+  user_id: "5",
+  list_title: "jeffrey's concept",
+  list_desc: "Please describe your ride",
+  price: 25,
+  lat: 37.781661,
+  long: -122.411579,
+  street: "1061 Market St",
+  city: "San Francisco",
+  state: "CA",
+  zip: 94103
+)
+
+Listing.create!(
+  user_id: "6",
+  list_title: "yusef's concept",
+  list_desc: "Please describe your ride",
+  price: 25,
+  lat: 37.781661,
+  long: -122.411579,
+  street: "1061 Market St",
+  city: "San Francisco",
+  state: "CA",
+  zip: 94103
+)
 
 Image.create!(
   listing_id: "1",
@@ -87,9 +118,22 @@ Image.create!(
   url: "http://www.pedalroom.com/p/keirin-style-njs-track-bike-3874_1.jpg"
 )
 
-
-
 Image.create!(
   listing_id: "3",
-  url: "http://4.bp.blogspot.com/_gTopFkQNeqQ/SKXqfwI9CqI/AAAAAAAABXg/9NK4ZMlHlro/s1600-h/XR3i.jp"
+  url: "http://www.infobarrel.com/media/image/50497.png"
+)
+
+Image.create!(
+  listing_id: "4",
+  url: "http://www.pedalroom.com/p/cinelli-mash-parallax-2014-19265_1.jpg"
+)
+
+Image.create!(
+  listing_id: "5",
+  url: "http://38.media.tumblr.com/tumblr_lztf6igro61qzlu6jo1_500.png"
+)
+
+Image.create!(
+  listing_id: "6",
+  url: "http://www.infobarrel.com/media/image/50063.png"
 )
